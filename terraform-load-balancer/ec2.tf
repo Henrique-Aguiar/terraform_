e39f59241e48c8f00ff806d42f627acf571bc207
Henrique-Aguiar <hrikelr@hotmail.com>
2023-07-04 19:38:55 -0300

    creating instances

---
+++ terraform-load-balancer/ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "web" {
+  ami             = "ami-053b0d53c279acc90"
+  instance_type   = "t2.micro"
+  key_name        = "teste-ec2"
+  security_groups = [aws_security_group.server_ec2.name, aws_security_group.ssh.name]
+
+  user_data = file("${path.module}/scripts.sh")
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name = "teste-ec2"
+  security_groups = [aws_security_group.mysql.name, aws_security_group.ssh.name]
+
+  user_data = file("${path.module}/scripts2.sh")
+
+  tags = {
+    Name = "mysql"
+  }
+}
