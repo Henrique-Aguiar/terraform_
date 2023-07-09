f886bcb9a65b906ed24111bd5ec905122bd0f757
Henrique Dias Aguiar <hrikelr@hotmail.com>
2023-07-09 20:05:50 -0300

    fixing upload files

--- site-static-s3/s3.tf
+++ site-static-s3/s3.tf
@@ -25,26 +25,36 @@ resource "aws_s3_bucket_website_configuration" "dves_cloud" {
   }
 }
 
-resource "aws_s3_object" "dves_cloud" {
-  for_each = fileset("doctorcare/", "*")
-  bucket   = aws_s3_bucket.dves_cloud.id
-  key      = each.value
-  source   = "doctorcare/${each.value}"
-  etag     = filemd5("doctorcare/${each.value}")
-}
+#resource "aws_s3_object" "dves_cloud" {
+#  for_each = fileset("doctorcare/", "*")
+#  bucket   = aws_s3_bucket.dves_cloud.id
+#  key      = each.value
+#  source   = "doctorcare/${each.value}"
+#  etag     = filemd5("doctorcare/${each.value}")
+#}
 
-resource "aws_s3_object" "dves_cloud_assets" {
-  for_each = fileset("doctorcare/assets/", "*")
-  bucket   = aws_s3_bucket.dves_cloud.id
-  key      = "assets/${each.value}"
-  source   = "doctorcare/assets/${each.value}"
-  etag     = filemd5("doctorcare/assets/${each.value}")
-}
+#resource "aws_s3_object" "dves_cloud_assets" {
+#  for_each = fileset("doctorcare/assets/", "*")
+#  bucket   = aws_s3_bucket.dves_cloud.id
+#  key      = "assets/${each.value}"
+#  source   = "doctorcare/assets/${each.value}"
+#  etag     = filemd5("doctorcare/assets/${each.value}")
+#}
 
 resource "aws_s3_bucket_public_access_block" "dves_cloud" {
-  bucket = aws_s3_bucket.dves_cloud.id
+  bucket                  = aws_s3_bucket.dves_cloud.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "null_resource" "s3_upload" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "aws s3 cp --recursive doctorcare/ s3://${aws_s3_bucket.dves_cloud.bucket}"
+  }
 }
